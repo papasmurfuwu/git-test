e8d0bcd18e0da605eea73213cd177250b59e2737
papasmurfuwu <quentanwong@gmail.com>
2024-02-07 19:20:37 +0800

    Coded selection sort algorithm again

--- algorithms/selection-sort/script2.js
+++ algorithms/selection-sort/script2.js
@@ -1,24 +1,25 @@
 // Selection Sort 
 // Loop through array, switch place of smallest element in list if smaller than the first element 
 
-let ranArr = Array.from({length: 10}, () =>Math.floor(Math.random()*10)+1)
+let ranArr = Array.from({length: 100}, () =>Math.floor(Math.random()*10)+1)
 console.log(ranArr);
 
-let selectionSort = function(arr){
-    for (i=0; i<arr.length; i++){
-        min = i;
-        for(j=i+1; j<arr.length; j++){
+function selectionSort(arr){
+    for (i=0; i<arr.length;i++){
+        let min = i; 
+        for (j=i+1; j<arr.length; j++){
             if (arr[j] < arr[min]){
                 min = j;
             }
         }
+        // replace number if i (first element in loop isn't the smallest)
         if (min !== i){
-            let temp = arr[i]; // temporarily store first element of this loop 
+            let temp = arr[i];
             arr[i] = arr[min];
             arr[min] = temp;
         }
     }
-    return arr;
+    return arr 
 }
 
 console.log(selectionSort(ranArr));
